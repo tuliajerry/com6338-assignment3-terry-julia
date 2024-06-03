0fcced07f248d1a581c54ec235b1ef46ad16c324
tuliajerry <156032724+tuliajerry@users.noreply.github.com>
2024-06-02 21:40:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+var questionsArr = [
+  {
+    question: 'JavaScript is the best language.',
+    answer: true
+  },
+  {
+    question: 'The Earth is flat.',
+    answer: false
+  },
+  {
+    question: '2 + 2 equals 4.',
+    answer: true
+  },
+  {
+    question: 'The moon is made of cheese.',
+    answer: false
+  },
+  {
+    question: 'The sun rises in the west.',
+    answer: false
+  }
+];
 
+
+function runQuiz() {
+  let correctAnswers = 0; 
+
+  for (let i = 0; i < questionsArr.length; i++) {
+    let userAnswer = confirm(questionsArr[i].question);
+    
+    if (userAnswer === questionsArr[i].answer) {
+      correctAnswers++; 
+    }
+  }
+
+  let scorePercentage = (correctAnswers / questionsArr.length) * 100;
+
+  scorePercentage = Math.round(scorePercentage);
+
+  alert('Your final score is: ' + scorePercentage + '%');
+}
